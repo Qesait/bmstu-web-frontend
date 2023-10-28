69e7b97f5958e2b3b0b8b683b12e52ffe0c37b07
Qesait <kuzva2003@gmail.com>
2023-10-28 20:10:14 +0300

    simple two pages with router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Это наша стартовая страница</h1>
+  },
+  {
+    path: '/new',
+    element: <h1>Это наша страница с чем-то новеньким</h1>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ul>
+      <li>
+        <a href="/">Старт</a>
+      </li>
+      <li>
+        <a href="/new">Хочу на страницу с чем-то новеньким</a>
+      </li>
+    </ul>
+    <hr />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
