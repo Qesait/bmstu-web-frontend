2fefa1f3dabc0200fca2d8360e61c0611cc4fbee
Qesait <kuzva2003@gmail.com>
2023-11-20 21:36:45 +0300

    fixed imports for styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,5 @@
-// import React from 'react';
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
-import './scss/styles.scss';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
