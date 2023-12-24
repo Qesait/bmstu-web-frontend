2567f9c71c3ef8927fefaf5947c6610d4683693e
Qesait <kuzva2003@gmail.com>
2023-12-24 13:41:15 +0300

    the return of the root react really wanted root back I don't know how this works, it didn't before

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from "react-redux";
 import { store } from "./store";
 
-// document.body.style.height = '100vh';
-
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <BrowserRouter basename={`${import.meta.env.BASE_URL}`}>
       <App />
     </BrowserRouter>
-  </Provider>,
-  document.body
-)
+  </Provider>
+);
