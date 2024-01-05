3f2ca32ad6662a2a2f56d4997b0d3376d7583c15
Qesait <kuzva2003@gmail.com>
2024-01-05 16:19:55 +0300

    extra pages removed

--- src/pages/ContainerEdit.tsx
+++
@@ -1,7 +0,0 @@
-const ContainerEdit = () => {
-    return (
-        <h3 className="text-center">Редактирование контейнера</h3>
-    )
-}
-
-export default ContainerEdit
--- src/pages/ContainersTable.tsx
+++
@@ -1,7 +0,0 @@
-const ContainerTable = () => {
-    return (
-        <h3 className="text-center">Таблица для управления контейнерами</h3>
-    )
-}
-
-export default ContainerTable
